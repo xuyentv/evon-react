0bb0f6973f40d1682de37791e229fe0022b57746
xuyentv <xuyentv@infoplusvn.com>
2025-09-08 15:35:51 +0700

    init project

---
+++ evon-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import HomePage from "./HomePage/HomePage.jsx";
+
+function App() {
+
+    return (
+        <>
+            <h1>Welcome to my app</h1>
+            <div>
+                <HomePage/>
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ evon-project/src/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+        <>
+            <button type="submit">Button</button>
+        </>
+    )
+}
+
+export default HomePage;
